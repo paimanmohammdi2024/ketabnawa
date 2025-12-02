a9761e6fd001b7f9b41ec2b109883fd98c719688
Ali Alidad <alialidad540@gmail.com>
2025-12-02 16:48:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8459868664-fdc34",
+  "appId": "1:837420376202:web:4620bcf899f785c5990d73",
+  "apiKey": "AIzaSyD7iYHO5XzCLRr0vf5PzRqlP3FL5CB0VL4",
+  "authDomain": "studio-8459868664-fdc34.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "837420376202"
+};
